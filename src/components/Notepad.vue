c5916742a8eca4d4bee6940e8b2b5ed4abf32b70
evasyukov <egor.vasyukov1@gmail.com>
2024-03-22 13:24:56 +0700

    create notepad.vue and add layout page

---
+++ src/components/Notepad.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <textarea
+      v-model="note"
+      @input="saveNote"
+      placeholder="Введите текст..."
+    ></textarea>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
